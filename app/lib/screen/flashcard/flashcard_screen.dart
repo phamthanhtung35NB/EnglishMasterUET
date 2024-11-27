805eb11575a07e24fd60829bf9d616ad96013485
22021189_DinhHoangNam <124755171+hnamkk@users.noreply.github.com>
2024-11-28 01:57:35 +0700

    l6

--- app/lib/screen/flashcard/flashcard_screen.dart
+++ app/lib/screen/flashcard/flashcard_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
+import 'package:provider/provider.dart';
 import '../../data/words_data.dart';
 import '../../widgets/flashcard.dart';
+import '../home_screen.dart';
 import 'result_screen.dart';
 
 class FlashcardScreen extends StatefulWidget {
@@ -23,13 +25,13 @@ class _FlashcardScreenState extends State<FlashcardScreen> {
   @override
   Widget build(BuildContext context) {
     final words =
-    topicWords[widget.topic]!; // Lấy danh sách từ vựng theo chủ đề
+        topicWords[widget.topic]!; // Lấy danh sách từ vựng theo chủ đề
     final currentWord = words[currentIndex]; // Lấy từ hiện tại
 
     void checkAnswer(String input) {
       setState(() {
         isAnswerCorrect =
-        (input.trim().toLowerCase() == currentWord.word.toLowerCase());
+            (input.trim().toLowerCase() == currentWord.word.toLowerCase());
         if (isAnswerCorrect) correctAnswers++;
       });
 
@@ -43,23 +45,21 @@ class _FlashcardScreenState extends State<FlashcardScreen> {
         ),
       );
 
-      // Delay before going to next word (reduced to 1 second)
+      // Delay trước khi chuyển từ
       Future.delayed(const Duration(milliseconds: 800), () {
         Navigator.pop(context); // Close the dialog
         if (currentIndex < words.length - 1) {
           setState(() {
             currentIndex++;
           });
-          cardKey = GlobalKey<
-              FlipCardState>(); // Reset the card key to show the front side.
+          cardKey = GlobalKey<FlipCardState>();
         } else {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ResultScreen(
-                  correctAnswers: correctAnswers,
-                  totalQuestions: words.length,
-                )),
+          context.read<AppState>().updateScreen(
+            'Result',
+            ResultScreen(
+              correctAnswers: correctAnswers,
+              totalQuestions: words.length,
+            ),
           );
         }
       });
@@ -70,67 +70,87 @@ class _FlashcardScreenState extends State<FlashcardScreen> {
       body: Center(
         child: currentIndex < words.length
             ? Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlipCard(
-              key: cardKey, // Đảm bảo cardKey được gán cho FlipCard
-              front: Flashcard(text: currentWord.word),
-              back: Flashcard(text: currentWord.meaning),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                TextEditingController inputController =
-                TextEditingController();
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text("Enter the word"),
-                    content: TextField(
-                      controller: inputController,
-                      decoration:
-                      const InputDecoration(hintText: "Type here"),
-                    ),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                          checkAnswer(inputController
-                              .text); // Kiểm tra câu trả lời khi người dùng nhấn Submit
-                        },
-                        child: const Text("Submit"),
-                      ),
-                    ],
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FlipCard(
+                    key: cardKey, // Đảm bảo cardKey được gán cho FlipCard
+                    front: Flashcard(text: currentWord.word),
+                    back: Flashcard(text: currentWord.meaning),
                   ),
-                );
-              },
-              child: const Text("Continue"),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  correctAnswers++; // Tăng số câu trả lời đúng khi người dùng bỏ qua và biết từ này
-                  if (currentIndex < words.length - 1) {
-                    currentIndex++;
-                    cardKey = GlobalKey<
-                        FlipCardState>(); // Reset lại key của FlipCard để hiển thị mặt trước
-                  } else {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ResultScreen(
-                          correctAnswers: correctAnswers,
-                          totalQuestions: words.length,
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      TextEditingController inputController = TextEditingController();
+                      bool isEmpty = false;
+                      showDialog(
+                        context: context,
+                        builder: (context) => StatefulBuilder(
+                          builder: (context, setState) {
+                            return AlertDialog(
+                              title: const Text("Enter the word"),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  TextField(
+                                    controller: inputController,
+                                    decoration: const InputDecoration(hintText: "Type here"),
+                                  ),
+                                  if (isEmpty) // Hiển thị thông báo nếu input rỗng.
+                                    const Padding(
+                                      padding: EdgeInsets.only(top: 8.0),
+                                      child: Text(
+                                        "Please enter a word",
+                                        style: TextStyle(color: Colors.red),
+                                      ),
+                                    ),
+                                ],
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    if (inputController.text.trim().isEmpty) {
+                                      // Nếu input rỗng, cập nhật trạng thái và hiển thị thông báo.
+                                      setState(() {
+                                        isEmpty = true;
+                                      });
+                                    } else {
+                                      Navigator.pop(context);
+                                      checkAnswer(inputController.text); // Gọi hàm kiểm tra.
+                                    }
+                                  },
+                                  child: const Text("Submit"),
+                                ),
+                              ],
+                            );
+                          },
                         ),
-                      ),
-                    );
-                  }
-                });
-              },
-              child: const Text("I already know this word"),
-            ),
-          ],
-        )
+                      );
+                    },
+                    child: const Text("Continue"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        correctAnswers++; // Tăng số câu trả lời đúng khi người dùng bỏ qua và biết từ này
+                        if (currentIndex < words.length - 1) {
+                          currentIndex++;
+                          cardKey = GlobalKey<
+                              FlipCardState>(); // Reset lại key của FlipCard để hiển thị mặt trước
+                        } else {
+                          context.read<AppState>().updateScreen(
+                                'Result',
+                                ResultScreen(
+                                  correctAnswers: correctAnswers,
+                                  totalQuestions: words.length,
+                                ),
+                              );
+                        }
+                      });
+                    },
+                    child: const Text("I already know this word"),
+                  ),
+                ],
+              )
             : const CircularProgressIndicator(),
       ),
     );
